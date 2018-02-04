9f0d96986e49c30cc4532b9c6ff361e1c13abba8
Andrey Babak <ababak@gmail.com>
2018-02-04 14:14:46 +0200

    Support URLs instead of traditional file paths
    Closing issue #2

--- openbash/__init__.py
+++ openbash/__init__.py
@@ -1,6 +1,13 @@
-from fman import DirectoryPaneCommand
 import subprocess
+from fman import DirectoryPaneCommand
+from fman.url import splitscheme, as_human_readable
 
 class OpenBash(DirectoryPaneCommand):
     def __call__(self):
-        subprocess.call('C:/Program Files/Git/git-bash.exe --cd="{cd}"'.format(cd=self.pane.get_path()))
+        url = self.pane.get_path()
+        scheme, path = splitscheme(url)
+        if scheme != 'file://':
+            show_alert('Not supported.')
+			return
+        local_path = as_human_readable(url)
+        subprocess.call('C:/Program Files/Git/git-bash.exe --cd="{cd}"'.format(cd=local_path))
