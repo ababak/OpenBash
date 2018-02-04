b857c916b252c72a3ddd91433c246b53401258c0
ababak <ababak@gmail.com>
2018-02-04 15:28:05 +0200

    Don't crash on unsupported platforms, display a dialog instead

--- openbash/__init__.py
+++ openbash/__init__.py
@@ -3,7 +3,8 @@ Open Bash window
 '''
 
 import subprocess
-from fman import DirectoryPaneCommand, show_alert
+import platform
+from fman import DirectoryPaneCommand, show_alert, run_application_command, get_application_commands
 from fman.url import splitscheme, as_human_readable
 
 class OpenBash(DirectoryPaneCommand):
@@ -17,5 +18,10 @@ class OpenBash(DirectoryPaneCommand):
             show_alert('Not supported.')
             return
         local_path = as_human_readable(url)
-        subprocess.call('C:/Program Files/Git/git-bash.exe --cd="{cd}"'.format(
-            cd=local_path))
+        if platform.system() == 'Windows':
+            subprocess.call('C:/Program Files/Git/git-bash.exe --cd="{cd}"'.format(
+                cd=local_path))
+        else:
+            show_alert(
+                "Sorry, this plugin is supposed to run on Windows. "
+                "Use the built-in 'Open terminal' command on other platforms.")
