2ed065142480300744a1be0fae20ed1785d5cf5d
ababak <ababak@gmail.com>
2018-02-04 14:42:55 +0200

    Lost imports

--- openbash/__init__.py
+++ openbash/__init__.py
@@ -1,13 +1,21 @@
+'''
+Open Bash window
+'''
+
 import subprocess
-from fman import DirectoryPaneCommand
+from fman import DirectoryPaneCommand, show_alert
 from fman.url import splitscheme, as_human_readable
 
 class OpenBash(DirectoryPaneCommand):
+    '''
+    Open Bash
+    '''
     def __call__(self):
         url = self.pane.get_path()
         scheme, path = splitscheme(url)
         if scheme != 'file://':
             show_alert('Not supported.')
-			return
+            return
         local_path = as_human_readable(url)
-        subprocess.call('C:/Program Files/Git/git-bash.exe --cd="{cd}"'.format(cd=local_path))
+        subprocess.call('C:/Program Files/Git/git-bash.exe --cd="{cd}"'.format(
+            cd=local_path))
